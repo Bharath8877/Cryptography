916bb70dd8ca82421465fffdeb8fff65faf3f5ae
Bharath <vsbharath98@gmail.com>
2019-12-28 14:06:52 +0530

    shift=3

---
+++ Caesar_Cipher.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Caesar_Cipher 
+{
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Enter the string to encrypt:");
+		Scanner s=new Scanner(System.in);
+		String enc=s.nextLine();
+		enc=enc.toLowerCase();
+		String dec="";
+		char c;
+		for(int i=0;i<enc.length();i++)
+		{
+			c=enc.charAt(i);
+			if(c=='x')dec=dec+'A';
+			else if(c=='y')dec=dec+'B';
+			else if(c=='z')dec=dec+'C';
+			else if(c==' ')dec=dec+' ';
+			else dec=dec+(char)((c+3));
+		}
+		dec=dec.toUpperCase();
+		System.out.print(dec);
+	}
+}
